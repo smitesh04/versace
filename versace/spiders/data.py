cf46122cffacb860c2789c02822a52b27b3782dd
smitesh04 <smiteshparmar1@gmail.com>
2024-10-10 14:41:26 +0530

    Changes

--- versace/spiders/data.py
+++ versace/spiders/data.py
@@ -45,11 +45,9 @@ class DataSpider(scrapy.Spider):
                     cb_kwargs=row
                 )
             else:
-
                 headers = {
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'accept-language': 'en-US,en;q=0.9',
-                'cache-control': 'max-age=0',
                 'priority': 'u=0, i',
                 'user-agent': ua.random,
                 }
